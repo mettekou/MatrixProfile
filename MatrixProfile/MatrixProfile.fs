afe563547e60ff77ddd95aa4904bc863ecd2b1d2
Dylan Meysmans <contact@mettekou.com>
2022-03-09 13:19:32 +0100

    Complete API

--- MatrixProfile/MatrixProfile.fs
+++ MatrixProfile/MatrixProfile.fs
@@ -3,3 +3,10 @@ namespace MatrixProfile
 module MatrixProfile =
     
     let selfJoin = Mpx.selfJoin
+    
+    let similarityJoin = Mpx.similarityJoin
+    
+type MatrixProfile =
+    static member SelfJoin(series, windowSize, euclideanDistance, nJobs) = MatrixProfile.selfJoin series windowSize euclideanDistance nJobs
+    
+    static member SimilarityJoin(series, query, windowSize, euclideanDistance, nJobs) = MatrixProfile.similarityJoin series query windowSize euclideanDistance nJobs
